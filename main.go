20aba525f88a1b7b7fc15ee103eb4aa8202a30af
0x-Singularity <gmcguir2@students.kennesaw.edu>
2024-03-25 20:56:47 -0400

    set up logic for snippetCreate to only accept POST req's

--- main.go
+++ main.go
@@ -24,14 +24,24 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetCreate( w http.ResponseWriter, r *http.Request){
-  w.Write([]byte("lets create a snippet"))
+  // use r.Method to check whether the request is using Post or not.
+  if r.Method != "POST" {
+    // If it's not, use the w.WriteHeader() method to send a 405 status
+    // code and the w.Write() method to write a "Method not allowed" 
+    // response body. We then return from the function so that the 
+    // subsequent code is not executed.
+    w.WriteHeader(405)
+    w.Write([]byte("Method not allowed"))
+    return
+  }
+  w.Write([]byte("Create a new snippet"))
 }
 
 func main(){
   mux := http.NewServeMux()
   mux.HandleFunc("/", home)
   mux.HandleFunc("/snippet/view", snippetView)
-  mux.HandleFunc("snippet/create", snippetCreate)
+  mux.HandleFunc("/snippet/create", snippetCreate)
 
   log.Println("Starting server on port 8080")
   err := http.ListenAndServe(":8080", mux)
